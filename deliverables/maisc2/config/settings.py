893416d7ca6a976116aa70e6e2c245170f212ac4
flaviomosafi <156256160+flaviomosafi@users.noreply.github.com>
2025-11-24 15:55:22 -0300

    Add files via upload

--- deliverables/maisc2/config/settings.py
+++ deliverables/maisc2/config/settings.py
@@ -1,24 +1,25 @@
 import os
+import sys
 from pathlib import Path
-import sys 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 sys.path.append(str(BASE_DIR / "apps"))
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f!w7q2p==0tt%cjd8e#5v@ww*k0h+-y+i(34$3damrgb73#-+)'
+SECRET_KEY = 'django-insecure-!6e^)@#569!(hn#xs#c-sg3b61ogc-ziam+peq8io87a4r2$lk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
+
 
 # Application definition
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -26,20 +27,29 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.postgres',
 
-    #... apps created to me
+    #... apps created
+    'account.apps.AccountConfig',
     'core.apps.CoreConfig',
+    'library.apps.LibraryConfig',
+    'room.apps.RoomConfig',
+    'sendc2.apps.Sendc2Config',
+    'recognize.apps.RecognizeConfig',
+    'classify.apps.ClassifyConfig',
+    'statistic.apps.StatisticConfig',
+    'delivery.apps.DeliveryConfig',
+    'api.apps.ApiConfig',
+    'tasks.apps.TasksConfig',
 
-    #... apps instancied
+    #... apps extends
+    'channels',
     'social_django',
-    'taggit',
-    'bootstrap4',
+    'bootstrap5',
     'crispy_forms',
     'widget_tweaks',
+    'rest_framework',
+    'corsheaders',
 ]
-
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -69,22 +79,32 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'config.wsgi.application'
+#WSGI_APPLICATION = 'config.wsgi.application'
+ASGI_APPLICATION = 'config.asgi.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer'
+    }
+}
 
 # Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
 
 # Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
+# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -101,27 +121,29 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
 # Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+# https://docs.djangoproject.com/en/4.2/topics/i18n/
+
+LANGUAGE_CODE = 'pt-br'
+
+TIME_ZONE = 'America/Sao_Paulo'
+
 USE_I18N = True
-USE_L10N = True
+
 USE_TZ = True
 
+
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_URL = 'static/'
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT  = os.path.join(BASE_DIR, 'media_root/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root/')
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-LOGIN_REDIRECT_URL  = 'core:core_home'
-LOGOUT_REDIRECT_URL = 'core:core_home'
-LOGIN_URL           = 'core:core_home'
-LOGOUT_URL          = 'core:core_home'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
